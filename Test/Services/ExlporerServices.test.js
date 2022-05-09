c489bcf5879efa158d08ffee9bcee6ec02d04207
JorgeCanto3 <launchx09367@innovaccion.mx>
2022-05-09 12:57:57 -0500

    Testing

--- Test/Services/ExlporerServices.test.js
+++ Test/Services/ExlporerServices.test.js
@@ -4,7 +4,7 @@ describe("Test for ExplorerServices", () => {
     test("Requerimietno 1: Calcular la cantidad de explorers en las mission de node", () => {
         const explorers = [{ mission: "node" }]
         const explorersInNode = ExplorerServices.filterByMission(explorers, "node")
-        expect(explorersInNode.length).toBe(56)
+        expect(explorersInNode.length).toBe(1)
 
     })
 
